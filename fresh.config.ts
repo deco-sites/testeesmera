caf2dcdf0ba105ae95a21279863b31dc075362c6
Paulo Amorim <amorimpaulohenrique04@gmail.com>
2026-07-28 19:56:20 +0000

    Let's get started!

---
+++ fresh.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "$fresh/server.ts";
+import { plugins } from "deco/plugins/deco.ts";
+import manifest from "./manifest.gen.ts";
+
+export default defineConfig({
+  plugins: plugins({
+    manifest,
+  }),
+});
